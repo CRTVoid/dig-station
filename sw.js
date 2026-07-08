3d115b4b28d3054a700095e0bafdbcb178ac06b3
CRTVoid <37534893+CRTVoid@users.noreply.github.com>
2026-07-08 17:16:25 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'radio-cache-v1';
+const URLS = [
+  '/',
+  '/index.html',
+  '/vinyl.svg',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(URLS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
+});
